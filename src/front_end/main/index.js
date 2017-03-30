d86d144a07be78b332d53996f6427f5a61795ef4
Joe Nguyen <ntuandung93@gmail.com>
2017-03-30 16:53:21 +0300

    Setup Redux Promise middleware for React

--- src/front_end/main/index.js
+++ src/front_end/main/index.js
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import ReduxPromise from 'redux-promise';
 
 import reducers from './../reducers/index';
 
 import App from './../components/app';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
