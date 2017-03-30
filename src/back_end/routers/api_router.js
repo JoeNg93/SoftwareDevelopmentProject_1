12fc1d387683b66251eba4daaf1d6e3f33f741a4
Joe Nguyen <ntuandung93@gmail.com>
2017-03-30 21:15:27 +0300

    Update API routes

--- src/back_end/routers/api_router.js
+++ src/back_end/routers/api_router.js
@@ -13,8 +13,21 @@ router.get('/', (req, res) => {
 });
 
 router.get('/recipes', (req, res) => {
-  Recipes.find().then((recipes) => {
+  Recipe.find().then((recipes) => {
     res.send(recipes);
+  }).catch((err) => {
+    res.status(400).send();
+  });
+});
+
+router.get('/recipe/:id', (req, res) => {
+  const id = req.params.id;
+
+  Recipe.findOne({_id: id}).then((recipe) => {
+    if (!recipe) {
+      res.status(404).send();
+    }
+    res.send(recipe);
   }).catch((err) => {
     res.status(400).send();
   });
@@ -23,6 +36,40 @@ router.get('/recipes', (req, res) => {
 router.get('/ingredients', (req, res) => {
   Ingredient.find().then((ingredients) => {
     res.send(ingredients);
+  }).catch((err) => {
+    res.status(400).send();
+  });
+});
+
+router.get('/ingredient/:id', (req, res) => {
+  const id = req.params.id;
+
+  Ingredient.findOne({_id: id}).then((ingredient) => {
+    if (!ingredient) {
+      res.status(404).send():
+    }
+    res.send(ingredient);
+  }).catch((err) => {
+    res.status(400).send();
+  });
+});
+
+router.get('/categories', (req, res) => {
+  Category.find().then((categories) => {
+    res.send(categories);
+  }).catch((err) => {
+    res.status(400).send();
+  });
+});
+
+router.get('/category/:id', (req, res) => {
+  const id = req.params.id;
+
+  Category.findOne({_id: id}).then((category) => {
+    if (!category) {
+      res.status(404).send();
+    }
+    res.send(category);
   }).catch((err) => {
     res.status(400).send();
   });
