68623953332904df703a63710130613652c59ea0
Joe Nguyen <ntuandung93@gmail.com>
2017-03-31 13:53:05 +0300

    Update seed data

--- src/back_end/tests/seed.js
+++ src/back_end/tests/seed.js
@@ -1,17 +1,89 @@
-const { ObjectID } = require('mongoose');
+const { ObjectID } = require('mongodb');
 
 const { Category } = require('./../models/categories');
 const { Ingredient } = require('./../models/ingredients');
 const { Recipe } = require('./../models/recipes');
 const { User } = require('./../models/users');
 
+const categories = [
+  {
+    _id: new ObjectID(),
+    name: 'Meats'
+  },
+  {
+    _id: new ObjectID(),
+    name: 'Vegetables'
+  },
+  {
+    _id: new ObjectID(),
+    name: 'Dairy'
+  },
+  {
+    _id: new ObjectID(),
+    name: 'Spices'
+  },
+  {
+    _id: new ObjectID(),
+    name: 'Oils'
+  }
+];
+
 const recipes = [];
 
 const users = [];
 
-const ingredients = [];
-
-const categories = [];
+const ingredients = [
+  {
+    _id: new ObjectID(),
+    name: 'bacon',
+    category: categories[0]._id
+  },
+  {
+    _id: new ObjectID(),
+    name: 'ground beef',
+    category: categories[0]._id
+  },
+  {
+    _id: new ObjectID(),
+    name: 'onion',
+    category: categories[1]._id
+  },
+  {
+    _id: new ObjectID(),
+    name: 'chili pepper',
+    category: categories[1]._id
+  },
+  {
+    _id: new ObjectID(),
+    name: 'eggs',
+    category: categories[2]._id
+  },
+  {
+    _id: new ObjectID(),
+    name: 'cream',
+    category: categories[2]._id
+  },
+  {
+    _id: new ObjectID(),
+    name: 'chive',
+    category: categories[3]._id
+  },
+  {
+    _id: new ObjectID(),
+    name: 'garlic powder',
+    category: categories[3]._id
+  },
+  {
+    _id: new ObjectID(),
+    name: 'olive oil',
+    category: categories[4]._id
+  },
+  {
+    _id: new ObjectID(),
+    name: 'peanut oil',
+    category: categories[4]._id
+  }
+];
 
 const populateRecipes = (done) => {
   Recipe.remove({}).then(() => {
