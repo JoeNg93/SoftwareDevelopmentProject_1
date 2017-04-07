39c7fd60ec027d8b1918373133fe6e20b45539e1
Joe Nguyen <ntuandung93@gmail.com>
2017-04-07 13:44:42 +0300

    GET /recipe?ingredients= also returns totalIngredients and numOfIngredientsHave

--- src/back_end/routers/api_router.js
+++ src/back_end/routers/api_router.js
@@ -105,171 +105,194 @@ router.get('/recipe', (req, res) => {
       if (recipes.length == 0) {
         res.status(404).send();
       }
-      res.send({ recipes });
-    })
-    .catch((err) => {
-      res.status(400).send();
+      const promiseQueues = recipes.map((recipe) => {
+        return new Promise((resolve, reject) => {
+          const totalIngredients = recipe.ingredients.length;
+          const numOfIngredientsHave = recipe.ingredients.filter(ingredient => ingredients.indexOf(ingredient.name) != -1).length;
+          resolve(Object.assign({}, recipe, { totalIngredients, numOfIngredientsHave }));
+        });
+      });
+      return Promise.all(promiseQueues);
+      })
+        .then((recipes) => {
+          res.send({ recipes });
+        })
+        .catch(err => res.status(400).send(err));
     });
-});
 
-router.post('/images/upload', (req, res) => {
-  const localImagePath = files.image.path;
-  uploadImageToCDN(localImagePath)
-    .then(imageCDNInfo => res.send(imageCDNInfo))
-    .catch(err => res.status(400).send());
-});
-
-router.post('/recipe', (req, res) => {
-  const { fields } = req;
-
-  let recipe = {
-    name: fields.name,
-    cookingTime: fields.cookingTime || 0,
-    image: fields.image,
-    numOfMeals: fields.numOfMeals || 1,
-    instructions: fields.instructions || []
-  };
-
-  const ingredients = fields.ingredients;
-  addIngredientName(ingredients)
-    .then((ingredientsWithName) => {
-      Object.assign(recipe, { ingredients: ingredientsWithName });
-      recipe = new Recipe(recipe);
-      return recipe.save();
-    })
-    .then(doc => res.send({ recipe: doc }))
-    .catch(err => res.status(400).send());
-
-});
-
-function addIngredientName(ingredients) {
-  const promiseQueues = [];
-  ingredients.forEach((ingredient) => {
-    promiseQueues.push(Ingredient.findOne({ _id: ingredient._id }).then((doc) => {
-      if (!doc) return Promise.reject('id not found');
-      return Object.assign({}, ingredient, { _id: doc._id, name: doc.name });
-    }));
-  });
-
-  return Promise.all(promiseQueues);
-}
-
-function uploadImageToCDN(localImagePath) {
-  return new Promise((resolve, reject) => {
-    cloudinary.uploader.upload(localImagePath, function handleResponse(response) {
-      let { public_id, version, format } = response;
-
-      const image = {
-        _id: public_id,
-        versionId: `v${version}`,
-        imageType: format
-      };
-
-      resolve(image);
-    });
-  });
-}
-
-router.get('/ingredients', (req, res) => {
-  Ingredient.find().then((ingredients) => {
-    res.send({ ingredients });
-  }).catch((err) => {
-    res.status(400).send();
-  });
-});
-
-router.get('/ingredient/:id', (req, res) => {
-  const id = req.params.id;
-
-  Ingredient.findOne({ _id: id }).then((ingredient) => {
-    if (!ingredient) {
-      res.status(404).send();
-    }
-    res.send({ ingredient });
-  }).catch((err) => {
-    res.status(400).send();
-  });
-});
-
-router.post('/ingredient', (req, res) => {
-  const fields = req.fields;
-  const categoryID = fields.categoryID;
-
-  if (!ObjectID.isValid(categoryID)) {
-    res.status(400).send();
+  function mapProps(props1, props2) {
+    return function to(recipes) {
+      console.log(recipes);
+      const promiseQueues = recipes.map((recipe) => {
+        return new Promise((resolve, reject) => {
+          const totalIngredients = recipe.ingredients.length;
+          const numOfIngredientsHave = recipe.ingredients.filter(ingredient => ingredients.indexOf(ingredient.name) != -1).length;
+          resolve(Object.assign({}, recipe, { totalIngredients, numOfIngredientsHave }));
+        });
+      });
+      console.log(promiseQueues);
+      return Promise.all(promiseQueues);
+    };
   }
 
-  Category.findOne({ _id: categoryID }).then((category) => {
-    if (!category) {
-      res.status(404).send();
-    }
-    const ingredient = new Ingredient({
-      name: fields.name,
-      categoryID: category._id,
-      categoryName: category.name
+  router.post('/images/upload', (req, res) => {
+    const localImagePath = files.image.path;
+    uploadImageToCDN(localImagePath)
+      .then(imageCDNInfo => res.send(imageCDNInfo))
+      .catch(err => res.status(400).send());
+  });
+
+  function uploadImageToCDN(localImagePath) {
+    return new Promise((resolve, reject) => {
+      cloudinary.uploader.upload(localImagePath, function handleResponse(response) {
+        let { public_id, version, format } = response;
+
+        const image = {
+          _id: public_id,
+          versionId: `v${version}`,
+          imageType: format
+        };
+
+        resolve(image);
+      });
     });
-    return ingredient.save();
-  }).then((doc) => {
-    res.send({ ingredient: doc });
-  }).catch((err) => {
-    res.status(400).send();
+  }
+
+  router.post('/recipe', (req, res) => {
+    const { fields } = req;
+
+    let recipe = {
+      name: fields.name,
+      cookingTime: fields.cookingTime || 0,
+      image: fields.image,
+      numOfMeals: fields.numOfMeals || 1,
+      instructions: fields.instructions || []
+    };
+
+    const ingredients = fields.ingredients;
+    addIngredientName(ingredients)
+      .then((ingredientsWithName) => {
+        Object.assign(recipe, { ingredients: ingredientsWithName });
+        recipe = new Recipe(recipe);
+        return recipe.save();
+      })
+      .then(doc => res.send({ recipe: doc }))
+      .catch(err => res.status(400).send());
+
   });
 
-});
+  function addIngredientName(ingredients) {
+    const promiseQueues = [];
+    ingredients.forEach((ingredient) => {
+      promiseQueues.push(Ingredient.findOne({ _id: ingredient._id }).then((doc) => {
+        if (!doc) return Promise.reject('id not found');
+        return Object.assign({}, ingredient, { _id: doc._id, name: doc.name });
+      }));
+    });
 
-router.get('/categories', (req, res) => {
-  Category.find().then((categories) => {
-    res.send({ categories });
-  }).catch((err) => {
-    res.status(400).send();
+    return Promise.all(promiseQueues);
+  }
+
+  router.get('/ingredients', (req, res) => {
+    Ingredient.find().then((ingredients) => {
+      res.send({ ingredients });
+    }).catch((err) => {
+      res.status(400).send();
+    });
   });
-});
 
-router.get('/category/:id', (req, res) => {
-  const id = req.params.id;
+  router.get('/ingredient/:id', (req, res) => {
+    const id = req.params.id;
 
-  Category.findOne({ _id: id }).then((category) => {
-    if (!category) {
-      res.status(404).send();
+    Ingredient.findOne({ _id: id }).then((ingredient) => {
+      if (!ingredient) {
+        res.status(404).send();
+      }
+      res.send({ ingredient });
+    }).catch((err) => {
+      res.status(400).send();
+    });
+  });
+
+  router.post('/ingredient', (req, res) => {
+    const fields = req.fields;
+    const categoryID = fields.categoryID;
+
+    if (!ObjectID.isValid(categoryID)) {
+      res.status(400).send();
     }
-    res.send({ category });
-  }).catch((err) => {
-    res.status(400).send();
-  });
-});
 
-router.get('/category/:id/ingredients', (req, res) => {
-  const id = req.params.id;
+    Category.findOne({ _id: categoryID }).then((category) => {
+      if (!category) {
+        res.status(404).send();
+      }
+      const ingredient = new Ingredient({
+        name: fields.name,
+        categoryID: category._id,
+        categoryName: category.name
+      });
+      return ingredient.save();
+    }).then((doc) => {
+      res.send({ ingredient: doc });
+    }).catch((err) => {
+      res.status(400).send();
+    });
 
-  Category.findOne({ _id: id }).then((category) => {
-    if (!category) {
-      res.status(404).send();
-    }
-    return Ingredient.find({ categoryID: category._id.toHexString() });
-  }).then((ingredients) => {
-    if (ingredients.length == 0) {
-      res.status(404).send();
-    }
-    res.send({ ingredients });
-  }).catch((err) => {
-    res.status(400).send();
-  });
-});
-
-router.post('/category', (req, res) => {
-  const fields = req.fields;
-  const category = new Category({
-    name: fields.name
   });
 
-  category.save().then((doc) => {
-    res.send({ category: doc });
-  }).catch((err) => {
-    res.status(400).send();
+  router.get('/categories', (req, res) => {
+    Category.find().then((categories) => {
+      res.send({ categories });
+    }).catch((err) => {
+      res.status(400).send();
+    });
   });
-});
 
-module.exports = {
-  router,
-  addIngredientName
-};
+  router.get('/category/:id', (req, res) => {
+    const id = req.params.id;
+
+    Category.findOne({ _id: id }).then((category) => {
+      if (!category) {
+        res.status(404).send();
+      }
+      res.send({ category });
+    }).catch((err) => {
+      res.status(400).send();
+    });
+  });
+
+  router.get('/category/:id/ingredients', (req, res) => {
+    const id = req.params.id;
+
+    Category.findOne({ _id: id }).then((category) => {
+      if (!category) {
+        res.status(404).send();
+      }
+      return Ingredient.find({ categoryID: category._id.toHexString() });
+    }).then((ingredients) => {
+      if (ingredients.length == 0) {
+        res.status(404).send();
+      }
+      res.send({ ingredients });
+    }).catch((err) => {
+      res.status(400).send();
+    });
+  });
+
+  router.post('/category', (req, res) => {
+    const fields = req.fields;
+    const category = new Category({
+      name: fields.name
+    });
+
+    category.save().then((doc) => {
+      res.send({ category: doc });
+    }).catch((err) => {
+      res.status(400).send();
+    });
+  });
+
+  module.exports = {
+    router,
+    addIngredientName
+  };
