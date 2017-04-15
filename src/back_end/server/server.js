acc138e62f12d3d5d318893cc2bcfad8b7735236
Joe Nguyen <ntuandung93@gmail.com>
2017-04-15 22:45:06 +0300

    Restructure

--- src/back_end/server/server.js
+++ src/back_end/server/server.js
@@ -2,16 +2,49 @@ require('./../config/config');
 
 const path = require('path');
 const express = require('express');
+const sessions = require('client-sessions');
+const formidable = require('formidable');
+const Mailgun = require('mailgun-js');
+const MailComposer = require('nodemailer/lib/mail-composer');
 
-const { router } = require('./../routers/api_router');
+const { SESSION_KEY } = require('./../config/keyConfig');
+
+const { api_router } = require('./../routers/api_router');
+
+const { auth_router } = require('./../routers/auth_router');
 
 const app = express();
 
 app.set('port', 8765);
 
+app.use(sessions({
+  cookieName: 'session',
+  secret: SESSION_KEY,
+  duration: 2 * 60 * 60 * 1000,
+  activeDuration: 5 * 60 * 1000,
+}));
+
+const formidableConfig = (req, res, next) => {
+  let form = formidable.IncomingForm();
+  form.uploadDir = path.resolve(__dirname, '..', 'images');
+  form.keepExtensions = true;
+  form.parse(req, (err, fields, files) => {
+    if (err) {
+      return res.status(400).send();
+    }
+    req.fields = fields;
+    req.files = files;
+    next();
+  });
+};
+
+app.use(formidableConfig);
+
 app.use(express.static(path.resolve(__dirname, '..', '..', 'front_end', 'assets')));
 
-app.use('/api', router);
+app.use('/api', api_router);
+
+app.use('/auth', auth_router);
 
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, '..', '..', 'front_end', 'index.html'));
